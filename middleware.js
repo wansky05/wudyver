a0a4cb705037529ab71fe3ffb1d594d88d10c458
AyGemuy <abdmalikalqadri2001@gmail.com>
2025-08-29 21:17:38 +0800

    Next.js

--- middleware.js
+++ middleware.js
@@ -432,6 +432,7 @@ export async function middleware(req) {
 }
 
 export const config = {
+runtime: 'nodejs',
   matcher: [
     "/((?!api|_next/static|_next/image|favicon.ico|manifest.json|sw.js|workbox-.*).*)"
   ],
